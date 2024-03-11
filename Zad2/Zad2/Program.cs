dee12e7db001f5b46021c7054112bdfa124f9da3
s29418 <s29418@pjwstk.edu.pl>
2024-03-11 16:34:47 +0100

    Dodano metode liczaca srednia

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -5,5 +5,18 @@
         Console.WriteLine("druga modyfikacja - test");
 
         int[] tablica = { 1, 3, 5, 10, 8 };
+        Console.WriteLine(Average(tablica));
+    }
+    
+    public static int Average(int[] tab)
+    {
+        int average = 0;
+        foreach (int num in tab)
+        {
+            average += num;
+        }
+
+        average = average / tab.Length;
+        return average;
     }
 }
