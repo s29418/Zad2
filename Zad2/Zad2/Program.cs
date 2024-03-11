75e179c83da50555198ac821273b854a6919ef0f
s29418 <s29418@pjwstk.edu.pl>
2024-03-11 16:44:25 +0100

    Dodano metode max

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -6,6 +6,7 @@
 
         int[] tablica = { 1, 3, 5, 10, 8 };
         Console.WriteLine(Average(tablica));
+        Console.WriteLine(Max(tablica));
     }
     
     public static int Average(int[] tab)
@@ -15,8 +16,21 @@
         {
             average += num;
         }
-
+        
         average = average / tab.Length;
         return average;
+    }
+
+    public static int Max(int[] tab)
+    {
+        int max = 0;
+        foreach (int num in tab)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        return max;
     }
 }
